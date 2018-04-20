5cd8bd1385bce2b239fe497916797327f0f12ae4
Imrich Valach <imrich@hmn2.io>
2018-04-20 03:46:01 +0200

    added support for installing via setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
-setup(name='youtube-trip',
+setup(name='youtrip',
       version='0.1',
-      description="Randomly picks one of the suggested youtube videos. Repeats forever (if you want)",
-      long_description='Enter youtube video, enter how many steps you want to make into the rabbit hole. Sit back and watch where it leads. It also creates a log file of your journey.',
+      description="A script that randomly picks one of the suggested youtube videos. Repeats forever (if you want)",
+      long_description='Enter youtube video url, enter how many steps you want to make into the rabbit hole. Sit back and watch where it leads. It also creates a log file of your journey.',
       classifiers=[],
       keywords='youtube rabbit hole suggested videos',
       author='Imrich Valach',
       author_email='nightcrawler@centrum.sk',
       url='https://www.youtube.com/watch?v=oHg5SJYRHA0',
       license='MIT', # or any license you think is relevant
-      packages=['motion_control'],
+      packages=['youtrip'],
       zip_safe=False,
       install_requires=[
           # add here any tool that you need to install via pip 
@@ -20,7 +20,6 @@ setup(name='youtube-trip',
       entry_points="""
       # -*- Entry points: -*-
       [console_scripts]
-      runstepper = motion_control.mystepper6:main
-      moveit = motion_control.moveit:main
+      youtrip = youtrip.application:main
       """,
 )
